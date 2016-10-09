6b595c247f829f3883adbe8e44d6290bdc23473a
namecrz <namecrz@126.com>
2016-10-09 10:18:03 +0800

    config in corp [hello git] by 20161009

--- src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Caller.java
+++ src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Caller.java
@@ -3,7 +3,8 @@ package test.selfstarter.algorithm.A0000.gcd.greatest_common_divisor;
 public class A0000Caller {
 
 	public static void main(String[] args) {
-
+		System.out.println("Hello Git !");
+		
 	}
 	
 }
