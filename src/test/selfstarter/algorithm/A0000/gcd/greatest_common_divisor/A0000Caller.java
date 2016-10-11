7cb5f594fb1477ea8c4d607ad21ef12ff18527fe
namecrz <namecrz@126.com>
2016-10-11 09:19:57 +0800

    Version 0.07
    Signed-off-by: namecrz <namecrz@126.com>

--- src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Caller.java
+++ src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Caller.java
@@ -9,7 +9,7 @@ public class A0000Caller {
 		int mod = 2;
 		A0000Util u =new A0000Util(a, b, mod);
 		//A0000Util [a=486721269, b=481245436, gcd=1, mod=3, timer=243360634]
-		//A0000Util [a=486721269, b=481245436, gcd=1, mod=2, timer=15]
+		//A0000Util [a=486721269, b=481245436, gcd=1, mod=2, timer=14]
 		System.out.println(u.toString());
 			
 	}
