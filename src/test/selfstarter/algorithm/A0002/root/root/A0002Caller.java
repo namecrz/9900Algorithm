809a77c384f42f5f9632c7065262d367bd7f9c86
namecrz <namecrz@126.com>
2016-10-12 19:56:37 +0800

    V0.1.0
    Signed-off-by: namecrz <namecrz@126.com>

--- src/test/selfstarter/algorithm/A0002/root/root/A0002Caller.java
+++ src/test/selfstarter/algorithm/A0002/root/root/A0002Caller.java
@@ -3,8 +3,8 @@ package test.selfstarter.algorithm.A0002.root.root;
 public class A0002Caller {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git !");
-		
+		int sqr = (int)Math.sqrt(1999999999);
+		System.out.println("Hello Git !"+sqr);
 	}
 	
 }
