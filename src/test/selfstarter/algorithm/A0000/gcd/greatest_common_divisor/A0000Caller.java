31cacfcc7c58856861459b617e800aaad4b45857
namecrz <namecrz@126.com>
2016-10-12 18:44:18 +0800

    Ver 1.0.0
    Signed-off-by: namecrz <namecrz@126.com>

--- src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Caller.java
+++ src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Caller.java
@@ -4,14 +4,13 @@ public class A0000Caller {
 
 	public static void main(String[] args) {
 		
-		int a = 486721268;
-		int b = 8;
-		int mod = 4;
+		int a = 953245243;
+		int b = 312431246;
+		int mod = 0;
 		
-		A0000Util u =new A0000Util(a, b, mod);
-		//A0000Util [a=486721269, b=481245436, gcd=1, mod=3, timer=243360634]
-		//A0000Util [a=486721269, b=481245436, gcd=1, mod=2, timer=14]
-		System.out.println(u.toString());
+		A0000Util u =new A0000Util(a, b,mod);
+
+//		System.out.println(u.toString());
 
 			
 	}
--- src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Util.java
+++ src/test/selfstarter/algorithm/A0000/gcd/greatest_common_divisor/A0000Util.java
@@ -2,7 +2,7 @@ package test.selfstarter.algorithm.A0000.gcd.greatest_common_divisor;
 
 /*
  * greatest_common_divisor:
- * 
+ * @parameter mod:算法模式
  * 
  */
 /*
@@ -20,6 +20,8 @@ package test.selfstarter.algorithm.A0000.gcd.greatest_common_divisor;
  @override 重写
  */
 public class A0000Util {
+	
+	private String desc;
 
 	private int a;
 
@@ -42,6 +44,9 @@ public class A0000Util {
 
 	}
 
+	/*
+	 * @parameter mod :1
+	 */
 	public A0000Util(int a, int b, int mod) {
 
 		init(a, b, mod);
@@ -79,9 +84,11 @@ public class A0000Util {
 	private void gcdCal() {
 
 		if (mod == 0) {
-			// 更相减损数位移
+			// statistics
+			statistics();
 		} else if (mod == 1) {
-			// 更相减损数
+			// stein:更相减损数+位移1
+			gcdCal_LoopMinusNew();
 		} else if (mod == 2) {
 			// 辗转相除
 			gcdCal_LoopDivisor();
@@ -89,8 +96,11 @@ public class A0000Util {
 			// 循环最小数
 			gcdCal_LoopSmaller();
 		} else if (mod == 4) {
-			// 辗转相除位移
+			// 辗转相除+RightMove1
 			gcdCal_LoopDivisorNew();
+		} else if (mod == 5) {
+			// 更相减损术
+			gcdCal_LoopMinus();
 		} else {
 			// 初始化异常，打印mod设置
 		}
@@ -98,10 +108,24 @@ public class A0000Util {
 	}
 
 	/*
-	 * 循环较小数求最小公倍数 mod4 原理：从小到大便利每一种可能 Loop: i>min(ceil(larger)/2,smaller)
+	 * 统计
+	 * 
+	 */
+	
+	private void statistics(){
+		for (int i=1;i<=5;i++){
+			gcd =1;
+			mod =i;
+			timer=0;
+			gcdCal();
+		}
+	}
+	
+	/*
+	 * 循环较小数求最大公约数 mod3 原理：从小到大便利每一种可能 Loop: i>min(ceil(larger)/2,smaller)
 	 */
 	private void gcdCal_LoopSmaller() {
-
+		this.desc = "遍历算法";
 		int j = (int) ((Math.ceil(larger >> 1) < smaller) ? Math
 				.ceil(larger >> 1) : smaller);
 		for (int i = 1; i <= j; i++) {
@@ -110,43 +134,65 @@ public class A0000Util {
 			}
 			timer();
 		}
+		System.out.println(this.toString());
 
 	}
 
 	/*
-	 * 辗转相除求最小公倍数 mod2 原理：y%x=k...b; y=kx+b; gcd(y,x)=gcd(x,b) Loop:Recursion
+	 * 辗转相除求最大公约数 mod2 原理：y%x=k...b; y=kx+b; gcd(y,x)=gcd(x,b) Loop:Recursion
 	 * x%b==0
 	 */
 	private void gcdCal_LoopDivisor() {
-
+		this.desc = "辗转相除算法";
 		gcdCal_LoopDivisor(larger, smaller);
+		System.out.println(this.toString());
 
 	}
 
 	/*
-	 * 位移辗转相除求最小公倍数 mod3 原理：y%x=k...b; y=kx+b; gcd(y,x)=gcd(x,b) Loop:Recursion
+	 * 位移辗转相除求最大公约数 mod4 原理：y%x=k...b; y=kx+b; gcd(y,x)=gcd(x,b) Loop:Recursion
 	 * x%b==0
 	 */
 	private void gcdCal_LoopDivisorNew() {
-
+		this.desc = "位移辗转相除算法";
 		gcdCal_LoopDivisorNew(larger, smaller);
+		System.out.println(this.toString());
 
 	}
 
 	/*
-	 * 辗转相除求最小公倍数 mod2
+	 * 更相减损术 mod5 原理：y-x=b; gcd(y,x)=gcd(x,b) Loop:Recursion x==b
+	 */
+	private void gcdCal_LoopMinus() {
+		this.desc = "更相减损术算法";
+		gcdCal_LoopMinus(larger, smaller);
+		System.out.println(this.toString());
+
+	}
+
+	/*
+	 * 更相减损术位移 mod1 原理：y-x=b; gcd(y,x)=gcd(x,b) Loop:Recursion x==b
+	 */
+	private void gcdCal_LoopMinusNew() {
+		this.desc = "stein算法";
+		gcdCal_LoopMinusNew(larger, smaller);
+		System.out.println(this.toString());
+	}
+
+	/*
+	 * 辗转相除求最大公约数 mod2
 	 */
 	private void gcdCal_LoopDivisor(int l, int s) {
-
-		timer();
 
 		int m;
 
 		if (l % s == 0) {
-
+			timer();
 			gcd *= s;
 
 		} else {
+
+			timer();
 
 			m = l % s;
 
@@ -154,22 +200,19 @@ public class A0000Util {
 			gcdCal_LoopDivisor(s, m);
 
 		}
-
 	}
 
 	/*
-	 * 辗转相除求最小公倍数 mod2
+	 * 辗转相除位移求最大公约数 mod4
 	 */
 	private void gcdCal_LoopDivisorNew(int l, int s) {
-
-		timer();
 
 		int m;
 		int ll;
 		int ss;
 
 		if (l % s == 0) {
-
+			timer();
 			gcd *= s;
 
 		} else {
@@ -189,13 +232,11 @@ public class A0000Util {
 
 			} else if (l % 2 != 0 && s % 2 == 0) {
 
-				ll = Math.max(s >> 1, s);
-				ss = Math.min(s >> 1, s);
-
-				gcdCal_LoopDivisorNew(ll, ss);
+				gcdCal_LoopDivisorNew(l, s >> 1);
 
 			} else if (l % 2 != 0 && s % 2 != 0) {
 
+				timer();
 				m = l % s;
 
 				// 因为余数<除数
@@ -203,6 +244,74 @@ public class A0000Util {
 
 			}
 
+		}
+
+	}
+
+	/*
+	 * 更相减损术求最大公约数 mod5 原理：y-x=b; gcd(y,x)=gcd(x,b) Loop:Recursion x==b
+	 */
+	private void gcdCal_LoopMinus(int l, int s) {
+
+		int mi = l - s;
+		int ll;
+		int ss;
+
+		if (mi == 0) {
+			timer();
+			gcd = s;
+
+		} else if (s == mi) {
+			timer();
+			gcd = mi;
+
+		} else {
+
+			timer();
+			ll = Math.max(s, mi);
+			ss = Math.min(s, mi);
+
+			gcdCal_LoopMinusNew(ll, ss);
+		}
+
+	}
+
+	/*
+	 * 更相减损术位移求最大公约数 mod1 原理：y-x=b; gcd(y,x)=gcd(x,b) Loop:Recursion x==b
+	 */
+	private void gcdCal_LoopMinusNew(int l, int s) {
+
+		int mi = l - s;
+		int ll;
+		int ss;
+
+		if (mi == 0) {
+			timer();
+			gcd *= s;
+
+		} else if (s == mi) {
+			timer();
+			gcd *= mi;
+
+		} else {
+
+			if (l % 2 == 0 && s % 2 == 0) {
+				gcd *= 2;
+				gcdCal_LoopMinusNew(l >> 1, s >> 1);
+			} else if (l % 2 != 0 && s % 2 == 0) {
+				gcdCal_LoopMinusNew(l, s >> 1);
+			} else if (l % 2 == 0 && s % 2 != 0) {
+				ll = Math.max(l >> 1, s);
+				ss = Math.min(l >> 1, s);
+				gcdCal_LoopMinusNew(ll, ss);
+			} else if (l % 2 != 0 && s % 2 != 0) {
+
+				timer();
+				ll = Math.max(s, mi);
+				ss = Math.min(s, mi);
+				gcdCal_LoopMinusNew(ll, ss);
+
+			}
 		}
 
 	}
@@ -246,7 +355,7 @@ public class A0000Util {
 
 	public String toString() {
 		return "A0000Util [a=" + a + ", b=" + b + ", gcd=" + gcd + ", mod="
-				+ mod + ", timer=" + timer + "]";
+				+ mod + ", timer=" + timer +", desc="+desc+"]";
 	}
 
 }
